ed2e83284cef46f1e2b4cf9ca6b59b6366600cc3
Bartosz Gesina <bartoszg2008@gmail.com>
2025-08-04 11:18:07 +0200

    Header Components Formatted

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
     selector: 'app-root',
-
-    imports: [RouterOutlet],
-
+    standalone: true,
+    imports: [RouterOutlet, HeaderComponent],
     templateUrl: './app.html',
-    styleUrl: './app.scss'
+    styleUrls: ['./app.scss']
 })
 export class App {
-
+    title = "header-app";
 }
