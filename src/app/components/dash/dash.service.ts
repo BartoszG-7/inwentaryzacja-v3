479c86d4e388fed9024b6f3bba45168e8d8316bb
mm554 <murawskim994@gmail.com>
2025-08-04 09:49:54 +0200

    sadas

--- src/app/components/dash/dash.service.ts
+++ src/app/components/dash/dash.service.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 export class DashService {
     constructor(private readonly http: HttpClient) { }
     getData(): Observable<string> {
-        return this.http.get<string>('http://localhost:3000', new Object({
+        return this.http.post<string>('http://localhost:3000/data/', "", new Object({
             responseType: 'text'
         }));
 
