0e2acba1dfb7f7ff587894d63fadd53bd668928e
mm554 <murawskim994@gmail.com>
2025-08-05 09:39:22 +0200

    added example locations db

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [RouterOutlet, HeaderComponent, FooterComponent],
+    providers: [CookieService],
     templateUrl: './app.html',
     styleUrls: ['./app.scss']
 })
 export class App {
+    constructor(private cookieService: CookieService, private router: Router) { }
+    ngOnInit() {
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                console.log('Navigation ended:', event.url);
+            }
+        });
 
+    }
 }
