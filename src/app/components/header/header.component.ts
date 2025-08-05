9030e4a85d7a5fb4d4c2750bff7c4a11d0373b2d
mm554 <murawskim994@gmail.com>
2025-08-05 10:05:53 +0200

    login works

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit } from '@angular/core';  
+import { Component, OnInit } from '@angular/core';
 import { HeaderService } from './header.service';
 import { HttpClient } from '@angular/common/http';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [ RouterLink],
-    providers: [HeaderService, HttpClient],
+    imports: [RouterLink],
+    providers: [HeaderService, HttpClient, CookieService],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-    menuValue:boolean = false;
-    menu_icon: string = 'bi bi-list';
 
+    constructor(private readonly cookieService: CookieService, private readonly router: Router) { }
+    loginText: string = 'Login';
+    ngOnInit(): void {
+        if (this.cookieService.get('secret') === "d07f690d14a52002aa869e7b7e428bc79d49466141b85952a69009e36d8ef701") {
+            this.loginText = 'Logout';
+
+        } else {
+            this.loginText = 'Login';
+        }
+    }
+    menuValue: boolean = false;
+    menu_icon: string = 'bi bi-list';
+    loginHandler() {
+        this.closeMenu();
+        if (this.cookieService.get('secret') === "d07f690d14a52002aa869e7b7e428bc79d49466141b85952a69009e36d8ef701") {
+            this.cookieService.delete('secret');
+            this.loginText = 'Login';
+            this.router.navigate(['/login']);
+        }
+    }
     openMenu() {
         this.menuValue = !this.menuValue;
         this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
@@ -25,17 +44,7 @@ export class HeaderComponent implements OnInit {
         this.menu_icon = 'bi bi-list';
     }
 
-    constructor(private readonly HeaderService: HeaderService) { }
 
-    ngOnInit(): void {
-        this.HeaderService.getData().subscribe({
-            next: (data) => {
-                console.log('Data received:', data);
-            },
-            error: (error) => {
-                console.error('Error fetching data:', error);
-            }
-        });
 
-    }
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent,  FooterComponent],
+  imports: [HeaderComponent, FooterComponent],
+  providers: [CookieService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  constructor(private cookieService: CookieService, private router: Router) { }
+  ngOnInit(): void {
+    if (this.cookieService.get('secret') === 'd07f690d14a52002aa869e7b7e428bc79d49466141b85952a69009e36d8ef701') {
+      this.router.navigate(['/home']);
+    }
+  }
+  login(username: string, password: string): void {
+    if (username === 'admin' && password === 'admin') {
+      this.cookieService.set('secret', 'd07f690d14a52002aa869e7b7e428bc79d49466141b85952a69009e36d8ef701');
+      this.router.navigate(['/home']);
+    }
+  }
 }
