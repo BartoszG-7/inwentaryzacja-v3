4c48879e135c4f625c64407c1f2dcf6e892e4703
mm554 <murawskim994@gmail.com>
2025-08-06 09:38:50 +0200

    basic treebar

--- src/app/treebar/treebar.ts
+++ src/app/treebar/treebar.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { TreebarService } from './treebar.service';
+import { Treeexpander } from '../treeexpander/treeexpander';
 
 @Component({
   selector: 'app-treebar',
-  imports: [],
+  imports: [Treeexpander],
   templateUrl: './treebar.html',
   styleUrl: './treebar.scss'
 })
 export class Treebar implements OnInit {
   constructor(private treebarService: TreebarService) { }
+  data: Array<any> = [];
   ngOnInit(): void {
     this.treebarService.getNames().subscribe({
       next: (data: any) => {
-        console.log(data);
+
+        this.data = data; // Store the fetched data in the component's property
+        this.data.forEach((item: any) => {
+          item.projects = JSON.stringify(item.projects);
+         
+        });
+        
       },
       error: (err) => {
         console.error('Error fetching treebar names:', err);
