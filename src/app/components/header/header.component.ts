32e49ba9417ca0ef20a8b22c2f25477971b9e55d
mm554 <murawskim994@gmail.com>
2025-08-06 10:47:23 +0200

    treebar update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,11 +3,12 @@ import { Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { Treebar } from "../../treebar/treebar";
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, Treebar],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
