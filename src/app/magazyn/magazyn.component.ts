05eb312be7f7fc5384b34a68cb3f4765908d22a9
Bartosz Gesina <bartoszg2008@gmail.com>
2025-08-08 09:21:31 +0200

    routing, seperate buttons, and more folder changes. cleaned up code etc

--- src/app/magazyn/magazyn.component.ts
+++ src/app/magazyn/magazyn.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//import { MagazynService } from './magazyn.service';
 import { HeaderComponent } from '../components/header/header.component';
-import { FooterComponent } from "../components/footer/footer.component";
+import { Treebar } from '../treebar/treebar';
 
 @Component({
   selector: 'app-magazyn',
+  imports: [CommonModule, HeaderComponent, Treebar],
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
   templateUrl: './magazyn.component.html',
   styleUrl: './magazyn.component.scss'
 })
 export class MagazynComponent {
+//   locations: any[] = [];
+//   editing: string = '';
 
-}
+//   constructor(private readonly locationService: LocationService) {}
+
+//   ngOnInit(): void {
+//     this.locationService.getLocations().subscribe({
+//       next: (data: any) => {
+//         this.locations = data;
+//       }
+//     });
+//   }
+
+//   isDesktop(): boolean {
+//     return window.innerWidth > 900;
+//   }
+
+//   editData(id: string): void {
+//     this.editing = id;
+//   }
+
+//   saveData(id: string, name: string, address: string, tag: string, note: string): void {
+//     this.locationService.saveData(id, name, address, tag, note).subscribe({
+//       next: () => {
+//         this.ngOnInit();
+//       }
+//     });
+//     this.editing = '';
+//   }
+ }
