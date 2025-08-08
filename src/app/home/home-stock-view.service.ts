209c15211ff8e768fbb63a1cf0f6166c9ec7d445
mm554 <murawskim994@gmail.com>
2025-08-08 08:55:59 +0200

    moved stock view logic to service

--- src/app/home/home-stock-view.service.ts
+++ src/app/home/home-stock-view.service.ts
@@ -1,12 +1,49 @@
 import { Component, Injectable } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { switchMap } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class HomeStockViewService {
-constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
   getUnassignedDevices(): any {
     return this.httpClient.get('http://localhost:3000/device/unassigned');
   }
+  getUnassignedDevicesExperimental(): any {
+    return this.httpClient.get('http://localhost:3000/device/unassigned').pipe(switchMap((data: any) => {
+      return this.processUnassignedDevices(data);
+    }));
+  }
+  processUnassignedDevices(data: any): any[] {
+    let deviceList: any[] = [];
+
+    data.forEach((item: any) => {
+      if (item.deviceType != null) { //zabezpieczenie przed zlymi danymi w bazie
+
+        if (deviceList.length === 0) {
+          deviceList.push({ deviceTypeId: item.deviceType._id, deviceType: item.deviceType.name, counter: 1 });
+          return;
+        }
+        var brk: boolean = false;
+        deviceList.forEach(element => {
+
+          if (element.deviceTypeId === item.deviceType._id) {
+
+            element.counter++;
+            brk = true;
+
+          }
+
+
+        });
+        if (!brk) {
+
+          deviceList.push({ deviceTypeId: item.deviceType._id, deviceType: item.deviceType.name, counter: 1 });
+        }
+      }
+    });
+    return deviceList;
+  }
+
 }
