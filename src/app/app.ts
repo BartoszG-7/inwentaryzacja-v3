5e23a097669ce49d9d71aa4c78640e428ccd9aed
Bartosz Gesina <bartoszg2008@gmail.com>
2025-08-11 13:18:12 +0200

    removed excess imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent],
+    imports: [RouterOutlet],
     providers: [CookieService],
     templateUrl: './app.html',
     styleUrls: ['./app.scss']
