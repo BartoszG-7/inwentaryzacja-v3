8a4dcd4117013fa6397462cf71db1857c961ad45
Bartosz Gesina <bartoszg2008@gmail.com>
2025-08-11 10:58:13 +0200

    added template for button

---
+++ src/app/components/search-icon/search-icon.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-icon',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './search-icon.component.html',
+  styleUrls: ['./search-icon.component.scss']
+})
+export class SearchIconComponent {
+
+}
