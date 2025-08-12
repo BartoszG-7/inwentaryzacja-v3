ba4be204bdeae0975161eec6e908f014840f24ab
mm554 <murawskim994@gmail.com>
2025-08-12 13:01:24 +0200

    quick view fixes

--- backend-inwentaryzacja/src/modules/data/data.controller.ts
+++ backend-inwentaryzacja/src/modules/data/data.controller.ts
@@ -1,20 +1,35 @@
-import { Body, Controller, Delete, Get, Head, Header, Param, Patch, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Head,
+  Header,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { DataService } from './data.service';
 
 @Controller('/data')
 export class DataController {
-    constructor(private readonly dataService: DataService) { }
+  constructor(private readonly dataService: DataService) {}
 
-    /* @Get('inventory-list')
+  /* @Get('inventory-list')
      async getInventoryList() {
          return await this.dataService.inventoryList();
      }*/
-    @Get('treebar')
-    async getTreebar() {
-        return await this.dataService.treebar();
-    }
-    @Get('home')
-    async getHome() {
-        return await this.dataService.home();
-    }
+  @Get('treebar')
+  async getTreebar() {
+    return await this.dataService.treebar();
+  }
+  @Get('home')
+  async getHome() {
+    return await this.dataService.home();
+  }
+  @Post('add-project')
+  async addProject(@Body() projectData: any) {
+    return await this.dataService.addProject(projectData);
+  }
 }
--- backend-inwentaryzacja/src/modules/data/data.service.ts
+++ backend-inwentaryzacja/src/modules/data/data.service.ts
@@ -2,32 +2,84 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, RootFilterQuery } from 'mongoose';
 
-import { Device, DeviceDocument } from '../device/device.schema'
+import { Device, DeviceDocument } from '../device/device.schema';
 
 import { Project } from '../project/project.schema';
 import { Location } from '../location/location.schema';
 import { ProjectHistory } from '../project-history/project-history.schema';
 import { DeviceType } from '../device-type/device-type.schema';
+export enum projectHistoryEvents {
+  PROJECT_CREATED = 1,
+}
 @Injectable()
 export class DataService {
-    constructor(@InjectModel(Device.name) private deviceModel: Model<Device>, @InjectModel(Project.name) private ProjectModel: Model<Project>, @InjectModel(Location.name) private LocationModel: Model<Location>, @InjectModel(ProjectHistory.name) private ProjectHistoryModel: Model<ProjectHistory>, @InjectModel(DeviceType.name) private DeviceTypeModel: Model<DeviceType>) { }
+  constructor(
+    @InjectModel(Device.name) private deviceModel: Model<Device>,
+    @InjectModel(Project.name) private ProjectModel: Model<Project>,
+    @InjectModel(Location.name) private LocationModel: Model<Location>,
+    @InjectModel(ProjectHistory.name)
+    private ProjectHistoryModel: Model<ProjectHistory>,
+    @InjectModel(DeviceType.name) private DeviceTypeModel: Model<DeviceType>,
+  ) {}
 
-    async treebar(): Promise<any> {
+  async treebar(): Promise<any> {
+    return {
+      projects: await this.ProjectModel.find({}).select('name location').exec(),
+      locations: await this.LocationModel.find({}).select('name').exec(),
+    };
+  }
 
-        return ({ projects: await this.ProjectModel.find({}).select("name location").exec(), locations: await this.LocationModel.find({}).select("name").exec() });
-    }
-
-    /*  async inventoryList(): Promise<any> {
+  /*  async inventoryList(): Promise<any> {
   
   
           return (this.DeviceTypeModel.find({}).select("name"));
       }*/
-    async home(): Promise<any> {
+  async home(): Promise<any> {
+    // Get the 5 newest distinct project history records by project
+    const modified = await this.ProjectHistoryModel.aggregate([
+      {
+        $group: {
+          _id: '$project',
+          doc: { $first: '$$ROOT' },
+        },
+      },
+      { $replaceRoot: { newRoot: '$doc' } },
+      { $limit: 5 },
+    ]).sort({date: -1}).exec();
 
+    // Populate project and location fields
+    const populatedModified = await this.ProjectHistoryModel.populate(
+      modified,
+      [{ path: 'project', populate: { path: 'location', model: 'Location' } }],
+    );
 
-
-
-        return ({ stock: await this.deviceModel.find({ project: null }).populate("deviceType", ["name"]).where("deviceType").ne(null).select("name deviceType").exec(), modified: await this.ProjectHistoryModel.find({}).populate("project").populate({ path: "project", populate: { path: "location", model: "Location" } }).sort({ "date": -1 }).limit(5).exec() });
-    }
+    return {
+      stock: await this.deviceModel
+        .find({ project: null })
+        .populate('deviceType', ['name'])
+        .where('deviceType')
+        .ne(null)
+        .select('name deviceType')
+        .exec(),
+      modified: populatedModified,
+      //   modified: await this.ProjectHistoryModel.find({})
+      //     .populate('project')
+      //     .populate({
+      //       path: 'project',
+      //       populate: { path: 'location', model: 'Location' },
+      //     })
+      //     .sort({ date: -1 })
+      //     .limit(5)
+      //     .exec(),
+    };
+  }
+  async addProject(projectData: any): Promise<any> {
+    return this.ProjectHistoryModel.create({
+      type: projectHistoryEvents.PROJECT_CREATED,
+      date: new Date().toISOString(),
+      tag: '',
+      deviceId: '',
+      project: (await this.ProjectModel.create(projectData))._id,
+    });
+  }
 }
-
