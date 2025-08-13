92ea355bb50c6bcb73ca68758f9976d95f7cfb78
Bartosz Gesina <bartoszg2008@gmail.com>
2025-08-13 13:18:45 +0200

    basic project table from bootstrap

--- src/app/lokalizacje/lokalizacje-right-project/lokalizacje-right-project.component.ts
+++ src/app/lokalizacje/lokalizacje-right-project/lokalizacje-right-project.component.ts
@@ -10,4 +10,94 @@ import { CommonModule } from '@angular/common';
 })
 export class LokalizacjeRightProjectComponent {
   @Input() selectedId: any;
+
+  groupedRows = [
+    {
+      name: 'SERWER',
+      rows: [
+        {
+          snWamasoft: '',
+          snProducenta: '',
+          mac: '',
+          ip: '10.67.140.21',
+          brama: '10.67.140.254',
+          maska: '255.255.255.0',
+          anydesk: '',
+          dns1: '10.67.100.10',
+          dns2: '10.67.100.11',
+          serwer: '10.67.100.11',
+        }
+      ]
+    },
+    {
+      name: 'INFOKIOSK',
+      rows: [
+        {
+          snWamasoft: 'WMK24V/2025/0001',
+          snProducenta: '',
+          mac: '',
+          ip: '10.67.200.10',
+          brama: '10.67.200.254',
+          maska: '255.255.255.0',
+          anydesk: '',
+          dns1: '10.67.100.10',
+          dns2: '10.67.100.11',
+          serwer: '10.67.100.11',
+        }
+      ]
+    },
+    {
+      name: 'WAMA MED 43 C',
+      rows: [
+        {
+          snWamasoft: 'WM43/2025/0072',
+          snProducenta: '',
+          mac: '',
+          ip: '',
+          brama: '',
+          maska: '',
+          anydesk: '',
+          dns1: '',
+          dns2: '',
+          serwer: '',
+        },
+        {
+          snWamasoft: 'WM43C/2025/0073',
+          snProducenta: '',
+          mac: '',
+          ip: '54.87.204.98',
+          brama: '',
+          maska: '',
+          anydesk: '',
+          dns1: '',
+          dns2: '',
+          serwer: '',
+        },
+        // Add more rows as needed
+      ]
+    },
+    {
+      name: 'WAMA MED 21 C',
+      rows: [
+        {
+          snWamasoft: 'WM21/2025/0176',
+          snProducenta: '',
+          mac: '',
+          ip: '',
+          brama: '',
+          maska: '',
+          anydesk: '',
+          dns1: '',
+          dns2: '',
+          serwer: '',
+        },
+        // Add more rows as needed
+      ]
+    }
+  ];
+
+  copyCell(value: string) {
+    if (!value) return;
+    navigator.clipboard.writeText(value);
+  }
 }
